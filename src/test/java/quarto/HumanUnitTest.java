24c87b3622341069dfd33e6137adb2683766c3ad
Alexis <xyrodileas@gmail.com>
2012-12-05 11:43:06 +0100

    Ajout OrdiUnitTest et HumanUnitTest

---
+++ src/test/java/quarto/HumanUnitTest.java
@@ -0,0 +1,18 @@
+package quarto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexis
+ * Date: 05/12/12
+ * Time: 10:35
+ * To change this template use File | Settings | File Templates.
+ */
+import org.junit.Test;
+
+public class HumanUnitTest {
+
+    @Test
+    public void getPieceToPlay() {
+
+    }
+}
