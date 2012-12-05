4a26356929f4c81e48826c11b55aed2c9e061434
maijin <maijin@maijin-laptop>
2012-12-05 09:37:10 +0100

    Formattage de l'ensemble du code : test et source, ajout d'accolades, identations...

--- src/test/java/quarto/GridUnitTest.java
+++ src/test/java/quarto/GridUnitTest.java
@@ -7,7 +7,6 @@ package quarto;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.swing.Box;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,68 +20,67 @@ public class GridUnitTest {
     public void getTabPionTest() {
         List<Piece> pionsDisponibles = new ArrayList<Piece>();
         PionBox box = new PionBox(pionsDisponibles);
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         pionsDisponibles.add(p1);
         Grid g1 = new Grid(box);
-        
-        
+
+
         Piece[][] p2;
         p2 = g1.getTabPion();
     }
-    
+
     @Test
     public void getPionsDisponiblesTest() {
         List<Piece> pionsDisponibles = new ArrayList<Piece>();
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         pionsDisponibles.add(p1);
         PionBox box = new PionBox(pionsDisponibles);
-        
+
         Grid g1 = new Grid(box);
-        
+
         Assert.assertTrue(g1.getPionsDisponibles().contains(p1));
-        
+
     }
-    
-    @Test(expected=PionMemePlaceException.class)
-    public void ajoutPionTest() throws PionMemePlaceException{
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+
+    @Test(expected = PionMemePlaceException.class)
+    public void ajoutPionTest() throws PionMemePlaceException {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
-        PionBox box = new PionBox(Arrays.asList(p1,p2));
+        PionBox box = new PionBox(Arrays.asList(p1, p2));
         Grid g1 = new Grid(box);
-        Position posp1 = new Position(0,0);
-        Position posp2 = new Position(0,0); 
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(0, 0);
         g1.ajoutPion(posp1, p1);
         g1.ajoutPion(posp2, p2);
-        
-  
+
+
         // incomplet!! Il faut pouvoir tester la valeur de hist
         //Possède Une opération inutiles
         // Oublie de l'utilisation de IsFree, rendIndisponible
     }
 
     @Test
-    public void undoTest() throws PionMemePlaceException{
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+    public void undoTest() throws PionMemePlaceException {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
-        PionBox box = new PionBox(Arrays.asList(p1,p2));
+        PionBox box = new PionBox(Arrays.asList(p1, p2));
         Grid g1 = new Grid(box);
-        Position posp1 = new Position(0,0);
-        Position posp2 = new Position(0,1); 
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(0, 1);
         g1.ajoutPion(posp1, p1);
-        
+
         g1.ajoutPion(posp2, p2);
         g1.undo();
-        
 
-    }   
-    
-    public void isFreePositionTest() throws PionMemePlaceException
-    {
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+
+    }
+
+    public void isFreePositionTest() throws PionMemePlaceException {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
-        PionBox box = new PionBox(Arrays.asList(p1,p2));
+        PionBox box = new PionBox(Arrays.asList(p1, p2));
         Grid g1 = new Grid(box);
-        Position posp1 = new Position(0,0);
+        Position posp1 = new Position(0, 0);
         Assert.assertTrue(g1.isFreePosition(posp1));
         g1.ajoutPion(posp1, p1);
         Assert.assertFalse(g1.isFreePosition(posp1));
--- src/test/java/quarto/PionBoxUnitTest.java
+++ src/test/java/quarto/PionBoxUnitTest.java
@@ -1,19 +1,17 @@
 package quarto;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class PionBoxUnitTest {
 
-
     @Test
-    public void testRemove(){
-        Piece p1 = new Piece(Couleur.CLAIRE,Forme.CARREE,Taille.BASSE,Coeur.CREUSE);
+    public void testRemove() {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
-        PionBox box = new PionBox(Arrays.asList(p1,p2));
+        PionBox box = new PionBox(Arrays.asList(p1, p2));
         box.remove(p1);
         Assert.assertTrue(box.getPionsDisponibles().contains(p2));
         Assert.assertEquals(1, box.getPionsDisponibles().size());
@@ -21,7 +19,7 @@ public class PionBoxUnitTest {
     }
 
     @Test
-    public void testIsEmpty(){
+    public void testIsEmpty() {
         PionBox box = new PionBox(Collections.EMPTY_LIST);
         Assert.assertTrue(box.isEmpty());
     }
