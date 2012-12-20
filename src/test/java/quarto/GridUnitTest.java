842b9a896336c25d4e027c2f04d5536f3a655d4c
Alexis <xyrodileas@gmail.com>
2012-12-20 08:35:59 +0100

    Ajout des tests sur Grid manuellement car impossible de merge

--- src/test/java/quarto/GridUnitTest.java
+++ src/test/java/quarto/GridUnitTest.java
@@ -58,6 +58,7 @@ public class GridUnitTest {
         // incomplet!! Il faut pouvoir tester la valeur de hist
         //Possède Une opération inutiles
         // Oublie de l'utilisation de IsFree, rendIndisponible
+        // Attention aux nombres négatifs !
     }
 
     @Test
@@ -76,6 +77,7 @@ public class GridUnitTest {
 
     }
 
+    @Test
     public void isFreePositionTest() throws PionMemePlaceException {
         Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
         Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
@@ -88,21 +90,146 @@ public class GridUnitTest {
     }
 
     @Test
-    public void extractLineTest() throws PionMemePlaceException {
-        List<Piece> pionsDisponibles = new ArrayList<Piece>();
+    public void estGagneeLigneTest() throws PionMemePlaceException {
+
         Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
-        pionsDisponibles.add(p1);
-        PionBox box = new PionBox(pionsDisponibles);
+        Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p3 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p4 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
 
+        PionBox box = new PionBox(Arrays.asList(p1, p2, p3, p4));
         Grid g1 = new Grid(box);
-        Position x = new Position(1, 1);
-        g1.ajoutPion(x, p1);          // tabPion est pas initialisé à NULL
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(0, 1);
+        Position posp3 = new Position(0, 2);
+        Position posp4 = new Position(0, 3);
+        Assert.assertFalse(g1.estGagnee());
+        g1.ajoutPion(posp1, p1);
+        g1.ajoutPion(posp2, p2);
+        g1.ajoutPion(posp3, p3);
+        g1.ajoutPion(posp4, p4);
+        Assert.assertTrue(g1.estGagnee());
 
-        Line ligne1 = g1.extractLine(1);
 
-        Line ligne2 = new Line();
-        ligne2.add(p1);
+    }
+
+    @Test
+    public void estGagneeColonneTest() throws PionMemePlaceException {
+
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p3 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p4 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+
+        PionBox box = new PionBox(Arrays.asList(p1, p2, p3, p4));
+        Grid g1 = new Grid(box);
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(1, 0);
+        Position posp3 = new Position(2, 0);
+        Position posp4 = new Position(3, 0);
+        g1.ajoutPion(posp1, p1);
+        g1.ajoutPion(posp2, p2);
+        g1.ajoutPion(posp3, p3);
+        g1.ajoutPion(posp4, p4);
+        Assert.assertTrue(g1.estGagnee());
+    }
+
+    @Test
+    public void estGagneeDiagGaucheTest() throws PionMemePlaceException {
+
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p3 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p4 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+
+        PionBox box = new PionBox(Arrays.asList(p1, p2, p3, p4));
+        Grid g1 = new Grid(box);
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(1, 1);
+        Position posp3 = new Position(2, 2);
+        Position posp4 = new Position(3, 3);
+        g1.ajoutPion(posp1, p1);
+        g1.ajoutPion(posp2, p2);
+        g1.ajoutPion(posp3, p3);
+        g1.ajoutPion(posp4, p4);
+        Assert.assertTrue(g1.estGagnee());
+    }
+
+    @Test
+    public void estGagneeDiagDroiteTest() throws PionMemePlaceException {
+
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p3 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p4 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+
+        PionBox box = new PionBox(Arrays.asList(p1, p2, p3, p4));
+        Grid g1 = new Grid(box);
+        Position posp1 = new Position(0, 3);
+        Position posp2 = new Position(1, 2);
+        Position posp3 = new Position(2, 1);
+        Position posp4 = new Position(3, 0);
+        g1.ajoutPion(posp1, p1);
+        g1.ajoutPion(posp2, p2);
+        g1.ajoutPion(posp3, p3);
+        g1.ajoutPion(posp4, p4);
+        Assert.assertTrue(g1.estGagnee());
+
+    }
+
+    @Test
+    public void toStringTest() throws PionMemePlaceException {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        Piece p2 = new Piece(Couleur.FONCEE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        PionBox box = new PionBox(Arrays.asList(p1, p2));
+        Grid g1 = new Grid(box);
+        Position posp1 = new Position(0, 0);
+        Position posp2 = new Position(0, 1);
+        g1.ajoutPion(posp1, p1);
+
+        g1.ajoutPion(posp2, p2);
 
 
+        Assert.assertEquals("1 1 0 0 \n"
+                + "0 0 0 0 \n"
+                + "0 0 0 0 \n"
+                + "0 0 0 0 \n", g1.toString());
+    }
+
+    @Test
+    public void rendIndisponibleTest() {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+
+        PionBox box = new PionBox(Arrays.asList(p1));
+        Grid g1 = new Grid(box);
+        g1.rendIndisponible(p1);
+        Assert.assertTrue(box.isEmpty());
+
+    }
+
+    @Test
+    public void extractLineTest() {
+    }
+
+    @Test
+    public void extractColumnTest() {
+    }
+
+    @Test
+    public void extractSecondDiagonalTest() {
+    }
+
+    @Test
+    public void extractFirstDiagonalTest() {
+    }
+
+    @Test
+    public void isPatTest() {
+        Piece p1 = new Piece(Couleur.CLAIRE, Forme.CARREE, Taille.BASSE, Coeur.CREUSE);
+        PionBox box = new PionBox(Arrays.asList(p1));
+        Grid g1 = new Grid(box);
+        g1.rendIndisponible(p1);
+
+        Assert.assertTrue(g1.isPat());
     }
 }
